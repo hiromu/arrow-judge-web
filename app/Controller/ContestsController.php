7d44f965d2be4e0373621493f70bff292ed8f39f
Hiromu Yakura <hiromu1996@gmail.com>
2013-03-03 18:59:55 +0900

    remove license

--- app/Controller/ContestsController.php
+++ app/Controller/ContestsController.php
@@ -1,24 +1,4 @@
 <?php
-/**
- * Static content controller.
- *
- * This file will render views from views/pages/
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
 App::uses('AppController', 'Controller');
 
 class ContestsController extends AppController {
--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,24 +1,4 @@
 <?php
-/**
- * Static content controller.
- *
- * This file will render views from views/pages/
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
 App::uses('AppController', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
 
@@ -28,11 +8,6 @@ class UsersController extends AppController {
 	public $uses = array('User', 'Submission', 'Language');
 	public $components = array('Email', 'Session');
 	public $paginate = array('limit' => 50, 'order' => array('Submission.created' => 'desc'));
-
-	public function beforeFilter() {
-		parent::beforeFilter();
-		$this->Auth->flash['element'] = 'error';
-	}
 
 	public function index($id = null) {
 		if(!$id) {
