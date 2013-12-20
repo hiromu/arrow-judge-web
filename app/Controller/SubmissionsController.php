3b4f0db2b958ed5682db7e351f92b629dbc19186
Hiromu Yakura <hiromu1996@gmail.com>
2013-12-21 00:01:57 +0900

    change not to show error messages during contests

--- app/Controller/SubmissionsController.php
+++ app/Controller/SubmissionsController.php
@@ -37,16 +37,20 @@ class SubmissionsController extends AppController {
 			$this->redirect('index');
 		}
 		if($submission['Problem']['public'] == 0) {
-			if(!$this->Auth->user('admin') && $submission['Submission']['user_id'] != $this->Auth->user('id')) {
+			if(!$this->Auth->user('admin')) {
 				if($submission['Problem']['contest_id']) {
 					$contest = $this->Contest->findById($submission['Problem']['contest_id']);
 					if(!$contest || $contest['Contest']['user_id'] != $this->Auth->user('id')) {
 						if(strtotime($contest['Contest']['end']) > time()) {
-							$this->Session->setFlash('You are not permitted to view submissions of the others during contest', 'error');
-							$this->redirect('index');
+							if($submission['Submission']['user_id'] != $this->Auth->user('id')) {
+								$this->Session->setFlash('You are not permitted to view submissions of the others during contest', 'error');
+								$this->redirect('index');
+							} else if($submission['Submission']['error']) {
+								$submission['Submission']['error'] = '- You are not permitted to view error messages during contest -';
+							}
 						}
 					}
-				} else {
+				} else if($submission['Submission']['user_id'] != $this->Auth->user('id')) {
 					$this->Session->setFlash('You are not permitted to view submissions of the secret problem', 'error');
 					$this->redirect('index');
 				}
